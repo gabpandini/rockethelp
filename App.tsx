2c4544235b5d8f23ce4997580feaf0149651a201
Gabriela Pandini <gabriela.pandini@ambevtech.com.br>
2022-07-20 15:45:55 -0300

    Created new components and screens; Implemented routes;

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import React from 'react';
-import { SignIn } from './src/screens/signin';
 import { THEME } from './styles/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from './src/components/Loading';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -15,7 +15,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      { fontsLoaded ? <SignIn/> : <Loading/> }
+      { fontsLoaded ? <Routes/> : <Loading/> }
     </NativeBaseProvider>
   );
 }
--- src/screens/signin.tsx
+++ src/screens/signin.tsx
@@ -6,32 +6,32 @@ import { Input } from '../components/Input'
 import { Button } from '../components/Button'
 
 export function SignIn() {
-    const [name, setName] = useState('')
-    const [password, setPassword] = useState('')
-    const { colors } = useTheme()
+		const [name, setName] = useState('')
+		const [password, setPassword] = useState('')
+		const { colors } = useTheme()
 
-    return (
-        <VStack flex={1} alignItems='center' bg='gray.600' px={8} pt={24} >
-            <Logo/>
-            <Heading color='gray.600' fontSize='xl' mt={20} mb={6} >
-                Acesse sua conta
-            </Heading>
-            <Input 
-                placeholder='E-mail' 
-                mb={4}
-                InputLeftElement={ <Icon as={<Envelope color={colors.gray[300]} />} ml={4} /> }
-                onChangeText={setName}
-            />
-            <Input placeholder='Senha' 
-                InputLeftElement={ <Icon as={<Key color={colors.gray[300]} />} ml={4} /> }
-                mb={8}
-                secureTextEntry
-                onChangeText={setPassword}
-            />
-            <Button 
-                title='Entrar' 
-                w='full'
-            />
-        </VStack>
-    )
+		return (
+				<VStack flex={1} alignItems='center' bg='gray.600' px={8} pt={24} >
+						<Logo/>
+						<Heading color='gray.600' fontSize='xl' mt={20} mb={6} >
+								Acesse sua conta
+						</Heading>
+						<Input 
+								placeholder='E-mail' 
+								mb={4}
+								InputLeftElement={ <Icon as={<Envelope color={colors.gray[300]} />} ml={4} /> }
+								onChangeText={setName}
+						/>
+						<Input placeholder='Senha' 
+								InputLeftElement={ <Icon as={<Key color={colors.gray[300]} />} ml={4} /> }
+								mb={8}
+								secureTextEntry
+								onChangeText={setPassword}
+						/>
+						<Button 
+								title='Entrar' 
+								w='full'
+						/>
+				</VStack>
+		)
 }
